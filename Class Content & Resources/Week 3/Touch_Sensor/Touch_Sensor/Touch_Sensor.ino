ed39a4300e7d55a65bba42f951164696895ea833
Nolan Thomas <nolan.thomas@pcc.edu>
2021-01-26 12:48:04 -0800

    Update Touch_Sensor.ino

--- Class Content & Resources/Week 3/Touch_Sensor/Touch_Sensor/Touch_Sensor.ino
+++ Class Content & Resources/Week 3/Touch_Sensor/Touch_Sensor/Touch_Sensor.ino
@@ -1,12 +1,19 @@
+
+int pulsePin1 = 7;
+int pulsePin2 = 6;
+int pulsePin3 = 5;
 int key1 = 4;
 int key2 = 3;
 int key3 = 2;
-int ledRed = 10;
-int ledGreen = 9;
-int ledBlue = 8;
+int ledRed = 12;
+int ledGreen = 11;
+int ledBlue = 10;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(pulsePin1, OUTPUT);
+  pinMode(pulsePin2, OUTPUT);
+  pinMode(pulsePin3, OUTPUT);
   pinMode(key1, INPUT);
   pinMode(key2, INPUT);
   pinMode(key3, INPUT);
@@ -18,25 +25,34 @@ void setup() {
 }
 
 void loop() {
-  int keyValue1 = digitalRead(key1);
-  int keyValue2 = digitalRead(key2);
-  int keyValue3 = digitalRead(key3);
-  Serial.println(keyValue1);
 
-  if(keyValue1 == 0){
-    digitalWrite(ledRed, HIGH);
-  }else{
-    digitalWrite(ledRed, LOW);
+  capSense(pulsePin1, key1, ledRed, "RED");
+  capSense(pulsePin2, key2, ledGreen, "GREEN");
+  capSense(pulsePin3, key3, ledBlue, "BLUE");
+  Serial.println();
+
+}
+
+void capSense(int triggerPin, int keyPin, int ledPin, String LED) {
+
+  int trigReading = digitalRead(triggerPin);
+  if (trigReading == LOW) {
+    digitalWrite(triggerPin, HIGH);
+  } else {
+    digitalWrite(triggerPin, LOW);
   }
-  if(keyValue2 == 0){
-    digitalWrite(ledGreen, HIGH);
-  }else{
-    digitalWrite(ledGreen, LOW);
-  }
-  if(keyValue3 == 0){
-    digitalWrite(ledBlue, HIGH);
-  }else{
-    digitalWrite(ledBlue, LOW);
+
+  int duration = pulseIn(keyPin, HIGH, 100);
+  Serial.print(LED);
+  Serial.print("\t");
+  Serial.print(duration);
+  Serial.print("\t");
+
+  if (duration > 0) {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+  } else {
+    digitalWrite(ledPin, LOW);
   }
 
 }
